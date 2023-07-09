f6f4652adbc56a876cac1d1ab7286c6be68580f1
A Dhruva Krishnama Raju <48171972+DeeKay02@users.noreply.github.com>
2023-07-09 15:19:23 +0530

    Add remove liquidity and 2 way swap functions

--- hardhat/contracts/Exchange.sol
+++ hardhat/contracts/Exchange.sol
@@ -40,10 +40,7 @@ contract Exchange is ERC20 {
         uint256 ethReservePriorToFunctionCall = ethReserveBalance - msg.value;
         uint256 minTokenAmountRequired = (msg.value * tokenReserveBalance) / ethReservePriorToFunctionCall;
 
-        require(
-            amountOfToken >= minTokenAmountRequired,
-            "Insufficient amount of tokens provided"
-        );
+        require(amountOfToken >= minTokenAmountRequired, "Insufficient amount of tokens provided");
 
         // Transfer the token from the user to the exchange
         token.transferFrom(msg.sender, address(this), minTokenAmountRequired);
@@ -57,5 +54,67 @@ contract Exchange is ERC20 {
         _mint(msg.sender, lpTokensToMint);
 
         return lpTokensToMint;
+    }
+
+    // removeLiquidity allows users to remove liquidity from the exchange
+    function removeLiquidity(uint256 amountOfLPTokens) public returns (uint256, uint256) {
+        // Check that the user wants to remove >0 LP tokens
+        require(amountOfLPTokens > 0, "Amount of tokens to remove must be greater than 0");
+
+        uint256 ethReserveBalance = address(this).balance;
+        uint256 lpTokenTotalSupply = totalSupply();
+
+        // Calculate the amount of ETH and tokens to return to the user
+        uint256 ethToReturn = (ethReserveBalance * amountOfLPTokens) / lpTokenTotalSupply;
+        uint256 tokenToReturn = (getReserve() * amountOfLPTokens) / lpTokenTotalSupply;
+
+        // Burn the LP tokens from the user, and transfer the ETH and tokens to the user
+        _burn(msg.sender, amountOfLPTokens);
+        payable(msg.sender).transfer(ethToReturn);
+        ERC20(tokenAddress).transfer(msg.sender, tokenToReturn);
+
+        return (ethToReturn, tokenToReturn);
+    }
+
+    // getOutputAmountFromSwap calculates the amount of output tokens to be received based on xy = (x + dx)(y - dy)
+    function getOutputAmountFromSwap(uint256 inputAmount, uint256 inputReserve, uint256 outputReserve) public pure returns (uint256) {
+        require(inputReserve > 0 && outputReserve > 0, "Reserves must be greater than 0");
+
+        uint256 inputAmountWithFee = inputAmount * 99;
+
+        uint256 numerator = inputAmountWithFee * outputReserve;
+        uint256 denominator = (inputReserve * 100) + inputAmountWithFee;
+
+        return numerator / denominator;
+    }
+
+    // ethToTokenSwap allows users to swap ETH for tokens
+    function ethToTokenSwap(uint256 minTokensToReceive) public payable {
+        uint256 tokenReserveBalance = getReserve();
+        uint256 tokensToReceive = getOutputAmountFromSwap(
+            msg.value,
+            address(this).balance - msg.value,
+            tokenReserveBalance
+        );
+
+        require(tokensToReceive >= minTokensToReceive, "Tokens received are less than minimum tokens expected");
+
+        ERC20(tokenAddress).transfer(msg.sender, tokensToReceive);
+    }
+
+    // tokenToEthSwap allows users to swap tokens for ETH
+    function tokenToEthSwap(uint256 tokensToSwap, uint256 minEthToReceive) public {
+        uint256 tokenReserveBalance = getReserve();
+        uint256 ethToReceive = getOutputAmountFromSwap(
+            tokensToSwap,
+            tokenReserveBalance,
+            address(this).balance
+        );
+
+        require(ethToReceive >= minEthToReceive, "ETH received is less than minimum ETH expected");
+
+        ERC20(tokenAddress).transferFrom(msg.sender, address(this), tokensToSwap);
+
+        payable(msg.sender).transfer(ethToReceive);
     }
 }
